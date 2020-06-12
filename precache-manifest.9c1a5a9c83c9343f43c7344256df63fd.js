9b5b75b9476388e0e04cbb6116e253938cfcd4da
Aiden In <artistaiden@gmail.com>
2020-06-11 21:40:09 -0700

    Updates

---
+++ precache-manifest.9c1a5a9c83c9343f43c7344256df63fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95a64fcc50ef4fadf5365d1ea7f42d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55d09d83adff6759ee43",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f81867e0a461438ad9b6",
+    "url": "/static/js/2.4ef118ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ef118ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d09d83adff6759ee43",
+    "url": "/static/js/main.78703d51.chunk.js"
+  },
+  {
+    "revision": "24e8d5a84308de897e3a",
+    "url": "/static/js/runtime-main.c8f0954f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
